255f3d1d84206e3aad1cebddb8d66cc684d0d1ab
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-09-22 15:01:00 +0700

    init: stater code

---
+++ android/app/src/main/kotlin/com/ervalsa/papb_praktikum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ervalsa.papb_praktikum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PAPB Praktikum',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  Widget build(BuildContext) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
